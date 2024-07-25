f748e7486bc94ee94b0bcf71823357584c276827
Pannathorn <pttv48@gmail.com>
2024-07-25 08:11:10 +0700

    add more plugins and add more keymap

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -2,6 +2,7 @@ vim.keymap.set('n' , '<C-p>' , ":Telescope find_files<CR>" , {})
 vim.keymap.set('n' , '<leader>fg' , ":Telescope live_grep<CR>" , {})
 vim.keymap.set('n' , '<C-n>' , ":Neotree reveal left<CR>" , {})
 vim.keymap.set('n' , '<leader>ng' , ":Neotree toggle<CR>" , {})
+vim.keymap.set('n' , '<leader>hg' , ":nohlsearch<CR>" , {})
 
 vim.keymap.set('n' , '<C-k>' , ':wincmd k<CR>')
 vim.keymap.set('n' , '<C-j>' , ':wincmd j<CR>')
